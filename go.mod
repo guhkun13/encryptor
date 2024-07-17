e120bbfc2b42f5e0b6970df655dcbf7805aafe0b
Teguh Atma <guhkun13@gmail.com>
2024-07-17 16:55:29 +0700

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/guhkun13/encryptor
+
+go 1.21.5
---
+++ main.go
@@ -0,0 +1,70 @@
+package encryptor
+
+import (
+	"crypto/aes"
+	"crypto/cipher"
+	"fmt"
+)
+
+func Encrypt(plaintext []byte, key []byte, iv []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	paddedPlaintext := padPKCS7(plaintext)
+
+	ciphertext := make([]byte, aes.BlockSize+len(paddedPlaintext))
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], paddedPlaintext)
+
+	return ciphertext, nil
+}
+
+func padPKCS7(data []byte) []byte {
+	padding := aes.BlockSize - len(data)%aes.BlockSize
+	padtext := make([]byte, len(data)+padding)
+	copy(padtext, data)
+	for i := len(data); i < len(padtext); i++ {
+		padtext[i] = byte(padding)
+	}
+	return padtext
+}
+
+func Decrypt(ciphertext []byte, key []byte, iv []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		return nil, fmt.Errorf("ciphertext too short")
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], ciphertext[aes.BlockSize:])
+
+	plaintext, err := unpadPKCS7(ciphertext[aes.BlockSize:])
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
+
+func unpadPKCS7(data []byte) ([]byte, error) {
+	length := len(data)
+	if length == 0 {
+		return nil, fmt.Errorf("invalid padding: length is zero")
+	}
+	unpadding := int(data[length-1])
+	if unpadding > length || unpadding == 0 {
+		return nil, fmt.Errorf("invalid padding: padding byte out of range")
+	}
+	for i := length - unpadding; i < length; i++ {
+		if data[i] != byte(unpadding) {
+			return nil, fmt.Errorf("invalid padding: incorrect padding bytes")
+		}
+	}
+	return data[:length-unpadding], nil
+}
