34f7b7bf6c0bee590d359505cebf2d589f3dcbd8
Teguh Atma <guhkun13@gmail.com>
2024-07-17 17:52:18 +0700

    make it into one file

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package encryptor
 
 import (
 	"bufio"
+	"crypto/aes"
+	"crypto/cipher"
 	"fmt"
 	"math/rand"
 	"os"
@@ -42,4 +44,69 @@ func readLineFromFile(filename string, lineNumber int) (string, error) {
 
 	// If lineNumber is out of range
 	return "", fmt.Errorf("line number %d out of range", lineNumber)
+}
+
+// encrypt
+func Encrypt(plaintext []byte, key []byte, iv []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	paddedPlaintext := padPKCS7(plaintext)
+
+	ciphertext := make([]byte, aes.BlockSize+len(paddedPlaintext))
+	mode := cipher.NewCBCEncrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], paddedPlaintext)
+
+	return ciphertext, nil
+}
+
+func padPKCS7(data []byte) []byte {
+	padding := aes.BlockSize - len(data)%aes.BlockSize
+	padtext := make([]byte, len(data)+padding)
+	copy(padtext, data)
+	for i := len(data); i < len(padtext); i++ {
+		padtext[i] = byte(padding)
+	}
+	return padtext
+}
+
+// decrypt
+func Decrypt(ciphertext []byte, key []byte, iv []byte) ([]byte, error) {
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(ciphertext) < aes.BlockSize {
+		return nil, fmt.Errorf("ciphertext too short")
+	}
+
+	mode := cipher.NewCBCDecrypter(block, iv)
+	mode.CryptBlocks(ciphertext[aes.BlockSize:], ciphertext[aes.BlockSize:])
+
+	plaintext, err := unpadPKCS7(ciphertext[aes.BlockSize:])
+	if err != nil {
+		return nil, err
+	}
+
+	return plaintext, nil
+}
+
+func unpadPKCS7(data []byte) ([]byte, error) {
+	length := len(data)
+	if length == 0 {
+		return nil, fmt.Errorf("invalid padding: length is zero")
+	}
+	unpadding := int(data[length-1])
+	if unpadding > length || unpadding == 0 {
+		return nil, fmt.Errorf("invalid padding: padding byte out of range")
+	}
+	for i := length - unpadding; i < length; i++ {
+		if data[i] != byte(unpadding) {
+			return nil, fmt.Errorf("invalid padding: incorrect padding bytes")
+		}
+	}
+	return data[:length-unpadding], nil
 }
