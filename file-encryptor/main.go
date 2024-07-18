271c72abe98ccd4e21af7da99e86c4d2465bf361
Teguh Atma <guhkun13@gmail.com>
2024-07-18 13:54:39 +0700

    what is dis

---
+++ file-encryptor/main.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"bufio"
+	"encoding/base64"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
+
+	"github.com/guhkun13/encryptor"
+)
+
+var (
+	encryptionKey = []byte("4b5ff54a1bce726b8f50d7c3b1320471")
+	iv            = []byte("4a30d240c6ce75fb")
+)
+
+func main() {
+	// Define command-line flags for secret file path
+	secretFilePath := flag.String("secretFilePath", "", "Path to the secret file")
+	flag.Parse()
+
+	// Check if the secret file path is provided
+	if *secretFilePath == "" {
+		fmt.Println("Error: secret file path is not provided.")
+		flag.Usage()
+		return
+	}
+
+	// Set the output file path
+	outputFilePath := ".secret.encrypted.env"
+
+	// Open the secret file
+	file, err := os.Open(*secretFilePath)
+	if err != nil {
+		fmt.Printf("Error opening secret file: %v\n", err)
+		return
+	}
+	defer file.Close()
+
+	// Create the output file to write the encrypted content
+	outputFile, err := os.Create(outputFilePath)
+	if err != nil {
+		fmt.Printf("Error creating output file: %v\n", err)
+		return
+	}
+	defer outputFile.Close()
+
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		line := scanner.Text()
+
+		// Split the line into key and value
+		parts := strings.SplitN(line, "=", 2)
+		if len(parts) != 2 {
+			// If the line doesn't contain "=", write it as is
+			_, err = outputFile.WriteString(line + "\n")
+			if err != nil {
+				fmt.Printf("Error writing to output file: %v\n", err)
+				return
+			}
+			continue
+		}
+
+		key := parts[0]
+		value := parts[1]
+
+		// Encrypt the value
+		encryptedValue, err := encryptor.Encrypt([]byte(value), encryptionKey, iv)
+		if err != nil {
+			fmt.Printf("Error encrypting data: %v\n", err)
+			return
+		}
+
+		// Write the key and encrypted value to the output file
+		_, err = outputFile.WriteString(fmt.Sprintf("%s=%s\n", key, base64.StdEncoding.EncodeToString(encryptedValue)))
+		if err != nil {
+			fmt.Printf("Error writing to output file: %v\n", err)
+			return
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		fmt.Printf("Error reading secret file: %v\n", err)
+		return
+	}
+
+	fmt.Println("File successfully encrypted.")
+}
